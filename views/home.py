bd955ccfb5c58eaf985a50f1fc4e84367969b2ec
Marie Weiss <weissma8@students.zhaw.ch>
2026-02-25 11:12:32 +0100

    Email hinzugefügt

--- views/home.py
+++ views/home.py
@@ -6,8 +6,8 @@ st.title("Meine erste Streamlit App")
 
 """
 Diese App wurde von folgenden Personen entwickelt:
-- Anna Müller (anmu@zhaw.ch)
-- Peter Meier (peme@zhaw.ch)
+- Marie Weiss (weissma8@zhaw.students.ch)
+- Nicole Merola (merolnic@zhaw.students.ch)
 
 Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
 
